ad0983d72fc6bbbd1d3c7d8f610691be9ea7ae8a
sara elbrmawy <115745517+Saraelbrmawy@users.noreply.github.com>
2024-04-08 23:56:43 +0200

    Add files via upload

---
+++ TASK2/Assignment1/p1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    int *p1;
+    char *p2;
+    long long *p3;
+    printf("Before :\n");
+    printf("p1 : %d\n", p1); 
+    printf("p2 : %c\n", p2); 
+    printf("p3 : %lld\n", p3); 
+   
+    p1++;
+    p2++;
+    p3++;
+    printf("\nAfter :\n");
+    printf("p1 : %d\n", p1); 
+    printf("p2 : %c\n", p2); 
+    printf("p3 : %lld\n", p3); 
+
+    return 0;
+}
+//int size is 4 bytes ,so ++p1 will point to next intger
+//char size is 1 bytes ,so ++p2 will point to next character
+//long long size is 8 bytes, so ++p3 will point to next long long
